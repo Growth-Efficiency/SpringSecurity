9046ebc41b6d1e0e6cf16cba4e4b96b435e22621
dongHyo <kdhyo98@gmail.com>
2022-05-14 13:03:56 +0900

    feat: rememberMe 인증

--- kdhyo/basicsecurity/src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ kdhyo/basicsecurity/src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -5,12 +5,15 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
@@ -19,6 +22,12 @@ import org.springframework.security.web.authentication.logout.LogoutSuccessHandl
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	private final UserDetailsService userDetailsService;
+
+	public SecurityConfig(UserDetailsService userDetailsService) {
+		this.userDetailsService = userDetailsService;
+	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -30,6 +39,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		login(http);
 
 		logout(http);
+
+		http.rememberMe()
+			.rememberMeParameter("remember") // default:  remember-me
+			.tokenValiditySeconds(3600) //  default: 14일
+//			.alwaysRemember(true) // remember-me 기능이 활성화되지 않아도 항상 실행
+			.userDetailsService(userDetailsService)
+			;
 	}
 
 	private void login(HttpSecurity http) throws Exception {
