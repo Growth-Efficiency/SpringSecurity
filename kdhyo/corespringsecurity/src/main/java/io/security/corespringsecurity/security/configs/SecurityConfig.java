bb05adba19e68587141edf0cb57e85da9bf99579
dongHyo <kdhyo98@gmail.com>
2022-05-25 16:51:31 +0900

    feat: DB연동 인증처리 - CustomUserDetailsService

--- kdhyo/corespringsecurity/src/main/java/io/security/corespringsecurity/security/configs/SecurityConfig.java
+++ kdhyo/corespringsecurity/src/main/java/io/security/corespringsecurity/security/configs/SecurityConfig.java
@@ -1,5 +1,6 @@
 package io.security.corespringsecurity.security.configs;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
@@ -9,21 +10,21 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 @Slf4j
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	private final UserDetailsService userDetailsService;
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		String password = passwordEncoder().encode("1111");
-
-		auth.inMemoryAuthentication().withUser("user").password(password).roles("USER");
-		auth.inMemoryAuthentication().withUser("manager").password(password).roles("MANAGER", "USER");
-		auth.inMemoryAuthentication().withUser("admin").password(password).roles("ADMIN", "USER", "MANAGER");
+		auth.userDetailsService(userDetailsService);
 	}
 
 	@Bean
