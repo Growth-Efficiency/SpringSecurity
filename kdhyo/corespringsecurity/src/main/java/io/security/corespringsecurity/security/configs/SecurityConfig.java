e0c95bcc0f4ee3956210a8db457999f91bd706da
dongHyo <kdhyo98@gmail.com>
2022-05-26 16:34:43 +0900

    feat: 인증 필터 - AjaxAuthenticationFilter

--- kdhyo/corespringsecurity/src/main/java/io/security/corespringsecurity/security/configs/SecurityConfig.java
+++ kdhyo/corespringsecurity/src/main/java/io/security/corespringsecurity/security/configs/SecurityConfig.java
@@ -1,6 +1,7 @@
 package io.security.corespringsecurity.security.configs;
 
 import io.security.corespringsecurity.security.common.FormAuthenticationDetailsSource;
+import io.security.corespringsecurity.security.filter.AjaxLoginProcessingFilter;
 import io.security.corespringsecurity.security.handler.CustomAccessDeniedHandler;
 import io.security.corespringsecurity.security.provider.CustomAuthenticationProvider;
 import lombok.RequiredArgsConstructor;
@@ -8,6 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -19,6 +21,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -41,6 +44,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	}
 
 	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
+
+	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		http
 			.authorizeRequests()
@@ -59,11 +67,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.successHandler(authenticationSuccessHandler)
 			.failureHandler(authenticationFailureHandler)
 			.permitAll()
-			
+
 			.and()
 			.exceptionHandling()
 			.accessDeniedHandler(accessDeniedHandler())
+
+			.and()
+			.addFilterBefore(ajaxLoginProcessingFilter(), UsernamePasswordAuthenticationFilter.class)
 		;
+
+		http.csrf().disable();
 	}
 
 	@Bean
@@ -82,6 +95,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+	}
+
+	@Bean
+	public AjaxLoginProcessingFilter ajaxLoginProcessingFilter() throws Exception {
+		AjaxLoginProcessingFilter ajaxLoginProcessingFilter = new AjaxLoginProcessingFilter();
+		ajaxLoginProcessingFilter.setAuthenticationManager(authenticationManagerBean());
+
+		return ajaxLoginProcessingFilter;
 	}
 
 }
