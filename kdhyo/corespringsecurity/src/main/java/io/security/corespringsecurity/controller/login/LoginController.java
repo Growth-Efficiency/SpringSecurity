eb751eb0bdc230429bd71e01119531ff5be18d31
dongHyo <kdhyo98@gmail.com>
2022-05-30 14:43:11 +0900

    feat: Ajax 로그인 구현 & CSRF 설정

--- kdhyo/corespringsecurity/src/main/java/io/security/corespringsecurity/controller/login/LoginController.java
+++ kdhyo/corespringsecurity/src/main/java/io/security/corespringsecurity/controller/login/LoginController.java
@@ -9,6 +9,7 @@ import org.springframework.security.web.authentication.logout.SecurityContextLog
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -22,6 +23,14 @@ public class LoginController {
 		model.addAttribute("exception", exception);
 
 		return "user/login/login";
+	}
+
+	@RequestMapping(value = "/api/login")
+	public String ajaxLogin(@RequestParam(value = "error", required = false) String error,
+		@RequestParam(value = "exception", required = false) String exception, Model model) {
+		model.addAttribute("error", error);
+		model.addAttribute("exception", exception);
+		return "login";
 	}
 
 	@GetMapping("/logout")
